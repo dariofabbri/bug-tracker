970a8b19a7d1f8107c6c76b0ac60655dd08e4284
Dario Fabbri <dar.fabbri@gmail.com>
2014-12-02 19:46:59 +0100

    Progresses on bugs page.

--- app/scripts/controllers/bugs.js
+++ app/scripts/controllers/bugs.js
@@ -8,13 +8,13 @@
  * Controller of the bugTrackerApp
  */
 angular.module('bugTrackerApp')
-  .controller('BugsCtrl', function ($scope, usersFactory, projectsFactory) {
+  .controller('BugsCtrl', function ($scope, $modal, usersFactory, projectsFactory) {
 
     $scope.users = usersFactory.list();
     $scope.projects = projectsFactory.list();
 
-		$scope.filterProjects = function(projects, typed) {
-			return projects.filter(function(project) {
+		$scope.filterProjects = function (projects, typed) {
+			return projects.filter(function (project) {
 				var matchesCode = project.code && project.code.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
 				var matchesName = project.name && project.name.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
 				var matchesDescription = project.description && project.description.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
@@ -23,14 +23,36 @@ angular.module('bugTrackerApp')
 			});
 		};
 
-		$scope.filterUsers = function(users, typed) {
-			return users.filter(function(user) {
+		$scope.filterUsers = function (users, typed) {
+			return users.filter(function (user) {
 				var matchesAlias = user.alias && user.alias.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
 				var matchesName = user.name && user.name.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
 				var matchesSurname = user.surname && user.surname.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
 				var matchesEmail = user.email && user.email.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
 
 				return matchesAlias || matchesName || matchesSurname || matchesEmail;
+			});
+		};
+
+		$scope.openEditDialog = function (bug) {
+
+			var modalInstance = $modal.open({
+				templateUrl: 'views/editbugdialog.html',
+				controller: 'EditBugDialogCtrl',
+				resolve: {
+					bug: function () {
+						return bug;
+					},
+
+					isNew: function () {
+						return !bug;
+					}
+				}
+			});
+
+			modalInstance.result.then(function () {
+
+				$scope.bugs = usersFactory.list();
 			});
 		};
   });
