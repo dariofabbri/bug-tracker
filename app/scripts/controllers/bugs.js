4201da156ea653a6230b67341eb99e9949dfa7f8
Dario Fabbri <dar.fabbri@gmail.com>
2014-12-03 19:24:20 +0100

    Replaced typeaheads with Angular's select directives.

--- app/scripts/controllers/bugs.js
+++ app/scripts/controllers/bugs.js
@@ -13,27 +13,6 @@ angular.module('bugTrackerApp')
     $scope.users = usersFactory.list();
     $scope.projects = projectsFactory.list();
 
-		$scope.filterProjects = function (projects, typed) {
-			return projects.filter(function (project) {
-				var matchesCode = project.code && project.code.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
-				var matchesName = project.name && project.name.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
-				var matchesDescription = project.description && project.description.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
-
-				return matchesCode || matchesName || matchesDescription;
-			});
-		};
-
-		$scope.filterUsers = function (users, typed) {
-			return users.filter(function (user) {
-				var matchesAlias = user.alias && user.alias.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
-				var matchesName = user.name && user.name.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
-				var matchesSurname = user.surname && user.surname.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
-				var matchesEmail = user.email && user.email.toLowerCase().indexOf(typed.toLowerCase()) !== -1;
-
-				return matchesAlias || matchesName || matchesSurname || matchesEmail;
-			});
-		};
-
 		$scope.openEditDialog = function (bug) {
 
 			var modalInstance = $modal.open({
